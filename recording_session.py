ada07cd3866fd91f4555ffc7a4ca6df9050d3536
detrett <66321582+detrett@users.noreply.github.com>
2026-09-22 15:35:55 +0200

    Encapulsation added

--- recording_session.py
+++ recording_session.py
@@ -2,4 +2,9 @@ class RecordingSession:
     # A recording session belongs to one speaker and contains all the observations from that recording
     def __init__(self, speaker_profile, observations):
         self.speaker_profile = speaker_profile
-        self.observations = observations
+        self._observations = observations
+
+        # Encapsulating observation gives the analyzer controlled access without exposing the attribute directly
+    @property
+    def observations(self):
+        return self._observations
