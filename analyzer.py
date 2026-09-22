c51e32609427b30b2e5524a353573a8193100ccf
detrett <66321582+detrett@users.noreply.github.com>
2026-09-22 14:12:07 +0200

    Add average pitch analysis

--- analyzer.py
+++ analyzer.py
@@ -7,4 +7,18 @@ class Analyzer:
     def count_observations(self):
         return len(self.session.observations)
 
-    
+    # Function to calculate the average pitch
+    def average_pitch(self):
+        pitches = []
+
+        # Only adding observations where speech was detected
+        for observation in self.session.observations:
+            if observation.speech_present:
+                pitches.append(observation.pitch)
+
+        # Return nothing if no pitches were detected
+        if not pitches:
+            return None
+
+        # Return the average pitch otherwise
+        return sum(pitches) / len(pitches)
